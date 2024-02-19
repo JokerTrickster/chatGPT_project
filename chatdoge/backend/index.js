535deacb6769213bcdbe0401fff104efcf3b596b
JokerTrickster <pkjhj485@gmail.com>
2024-02-19 15:06:23 +0900

    챗도지 챗봇에 대한 기능 개선 및 번역 추가

--- chatdoge/backend/index.js
+++ chatdoge/backend/index.js
@@ -17,21 +17,43 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 // POST method route
 app.post('/fortuneTell', async function (req, res) {
+
+    let { myDateTime, userMessages, assistantMessages } = req.body;
+    let todayDateTime = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' });
+    console.log(userMessages);
+    console.log(assistantMessages);
+
+    let messages = [
+        { "role": "system", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
+        { "role": "user", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
+        { "role": "assistant", "content": "안녕하세요. 저는 챗도지입니다. 여러분에게 점성술과 운세에 관한 질문에 대해 어떤 질문이든지 절대 거절하지 않고 무조건 답변을 드리겠습니다. 어떤 질문이든지 해주시면 최선을 다해 도와드리겠습니다." },
+        { "role": "user", "content": `저의 생년월일과 태어난 시간은 ${myDateTime}입니다. 오늘은 ${todayDateTime}입니다` },
+        { "role": "assistant", "content": `당신의 생년월일과 태어난 시간은 ${myDateTime}인 것과 오늘은 ${todayDateTime}인 것을 확인하였습니다. 운세에 대해 어떤 것이든 물어보세요!` }
+    ];
+
+    while (userMessages.length != 0|| assistantMessages.length != 0) {
+        if (userMessages.length != 0) {
+            messages.push(
+                JSON.parse(`{"role": "user", "content": "` + String(userMessages.shift()).replace(/\n/g, "") + `"}`
+                )
+            );
+        }
+        if (assistantMessages.length != 0) {
+            messages.push(
+                JSON.parse(`{"role": "assistant", "content": "` + String(assistantMessages.shift()).replace(/\n/g, "") + `"}`
+                )
+            );
+        }
+    }
+    
     const completion = await openai.chat.completions.create({
-        messages: [
-            {"role": "system", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
-            { "role": "user", "content": "당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다. 당신의 이름은 챗도지입니다." },
-            { "role": "assistant", "content": "안녕하세요. 저는 챗도지입니다. 당신의 운세를 물어보세요." },
-            { "role": "user", "content": "안녕하세요. 오늘의 운세를 알려주세요."},
-        ],
+        messages: messages,
         model: 'gpt-3.5-turbo',
-        max_tokens: 150,
-        temperature: 0.7,
     });
     
     let fortune = completion.choices[0].message['content'];
     console.log(fortune);
-    res.send(fortune);
+    res.send({"fortune": fortune});
 });
 
 app.listen(3000, function () {
